c978a1123a68973fefb56fbd1751eb03f0cfe0ce
AKWANA686 <akwanaoliver@gmail.com>
2022-03-11 18:18:00 +0000

    a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 38; a < 48; a++)
+	for (a = 48; a < 58; a++)
 	{
-		for (b = 39; b < 48; b++)
+		for (b = 49; b < 58; b++)
 		{
-			for (c = 40; c < 48; c++)
+			for (c = 50; c < 58; c++)
 			{
 				if (c > b && b > a)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a != 45 || b != 46)
+					if (a != 55 || b != 56)
 					{
 						putchar(',');
 						putchar(' ');
