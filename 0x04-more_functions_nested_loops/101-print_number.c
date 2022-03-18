8bf95f7dee3f508be9b9a667e368f41604dc4a25
AKWANA686 <akwanaoliver@gmail.com>
2022-03-18 21:32:30 +0000

    a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@
 void print_number(int n)
 {
 	unsigned int a;
-	
+
 	if (n < 0)
 	{
 		a = -n;
@@ -19,7 +19,7 @@ void print_number(int n)
 	{
 		a = n;
 	}
-	
+
 	if (a / 10)
 	{
 		print_number(a / 10);
