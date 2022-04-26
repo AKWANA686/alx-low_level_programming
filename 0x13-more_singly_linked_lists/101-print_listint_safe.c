540e2ab8ac505b0b22c239af6f8f162e03c51b18
AKWANA686 <akwanaoliver@gmail.com>
2022-04-26 14:36:39 +0000

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,15 +1,70 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list
+ * free_listint - frees a linked list
  * @head: head of a list
- * Return: 0
+ * Return: no return.
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head == NULL)
-		exit(98);
+	listp_t *temp;
+	listp_t *curr;
 
-	return (0);
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nnodes = 0;
+	listint_t *hptr, *new, *add;
+
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+		
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listint(&hptr);
+				return (nnodes);
+			}
+		}
+		
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+
+	free_listint(&hptr);
+	return (nnodes);
 }
